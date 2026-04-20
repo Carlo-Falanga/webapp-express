12fcb03d68e29f780fc23de3f858c6155a15044c
Carlo-Falanga <carlofalanga7@gmail.com>
2026-04-20 12:53:48 +0200

    implement movies index and show routes with database queries

--- controllers/moviesControllers.js
+++ controllers/moviesControllers.js
@@ -1,12 +1,41 @@
 const connection = require("../database/connection")
 
 const index = (req, res) => {
-    res.send('Movies Index Page')
+    connection.query('SELECT * FROM movies', (err, results) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).json({ error: 'Database query failed' })
+        }
+        res.json(results)
+    })
 }
 
 const show = (req, res) => {
     const id = req.params.id
-    res.send(`Movie Show Page for movie with id: ${id}`)
+    
+    const moviesql = 'SELECT * FROM movies WHERE id = ?'
+
+    const rewiewsql = 'SELECT * FROM reviews WHERE movie_id = ?'
+
+    connection.query(moviesql, [id], (err, movieResults) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).json({ error: 'Database query failed' })
+        }
+        if (movieResults.length === 0) {
+            return res.status(404).json({ error: 'Movie not found' })
+        }
+        const movie = movieResults[0]
+
+        connection.query(rewiewsql, [id], (err, reviewResults) => {
+            if (err) {
+                console.error(err)
+                return res.status(500).json({ error: 'Database query failed' })
+            }
+            movie.reviews = reviewResults
+            res.json(movie)
+        })
+    })
 }
 
 module.exports = {
